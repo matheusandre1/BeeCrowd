74a7b98f3d9e3418b2233e2558630cb3b0aecbd9
Matheus Andre <matheusandr2@gmail.com>
2024-09-28 12:51:40 -0300

    new changes

---
+++ BeeCrowd/1007/Program.cs
@@ -0,0 +1,9 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int D = int.Parse(Console.ReadLine());
+
+int diferenca = ((A * B) - (C * D)) ;
+
+
+Console.WriteLine($"DIFERENCA = {diferenca}");
---
+++ BeeCrowd/1008/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+double Value_hours = double.Parse(Console.ReadLine());
+double salaryCalculation = hours * Value_hours;
+
+Console.WriteLine($"Number = {number}");
+Console.WriteLine($"SALARY = U$ {salaryCalculation.ToString("F2")}");
