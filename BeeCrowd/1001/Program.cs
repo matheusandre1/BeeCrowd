d0fb6ce1c4bc4db0fe56db4e1cfbe335e81f1bf1
Matheus Andre <matheusandr2@gmail.com>
2024-09-28 11:07:43 -0300

    feat: adicionando exercicios

---
+++ BeeCrowd/1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projetc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Int32.Parse(Console.ReadLine());
+            int B = Int32.Parse(Console.ReadLine());
+
+            int result = A + B;
+
+            Console.WriteLine($"x = {result}");
+        }
+    }
+}
---
+++ BeeCrowd/1002/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Globalization;
+
+double n = 3.14159;
+
+double raio = double.Parse(Console.ReadLine());
+
+double area = n * (raio * raio);
+
+Console.WriteLine($"A={area.ToString("F4",CultureInfo.InvariantCulture)}");
