6ba7337556365a6bb45b7379f9f885809024690a
Matheus Andre <matheusandr2@gmail.com>
2024-09-28 12:07:42 -0300

    new changes

---
+++ BeeCrowd/1006/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+
+double pesaA = 2;
+double pesoB = 3;
+double pesoC = 5;
+
+double media = ((pesaA * a) + (pesoB * b) + (pesoC * c)) / (pesaA + pesoB + pesoC);
+
+Console.WriteLine($"MEDIA = {media.ToString("F1",CultureInfo.InvariantCulture)}");
