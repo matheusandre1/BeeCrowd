416bc78bdaf0406951045fb91b18a7e2a3caad04
Matheus Andre <matheusandr2@gmail.com>
2024-10-11 22:18:34 -0300

    exercicios 1010 e 1011

---
+++ BeeCrowd/1010/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Globalization;
+
+string[] peca1 = Console.ReadLine().Split(' ');
+
+int codigo1 = int.Parse(peca1[0]);
+int qtd1 = int.Parse(peca1[1]);
+double preco1 = double.Parse(peca1[2]);
+
+string[] peca2 = Console.ReadLine().Split(' ');
+
+int codigo2 = int.Parse(peca2[0]);
+int qtd2 = int.Parse(peca2[1]);
+double preco2 = double.Parse(peca2[2]);
+
+double somaTotalPeca1 = qtd1 * preco1;
+double somaTotalPeca2 = qtd2 * preco2;
+double total = somaTotalPeca1 + somaTotalPeca2;
+Console.WriteLine($"VALOR A PAGAR: R$ {total.ToString("F2",CultureInfo.InvariantCulture)}");
+
---
+++ BeeCrowd/1011/Program.cs
@@ -0,0 +1,6 @@
+﻿
+int raio = int.Parse(Console.ReadLine());
+double pi = 3.14159;
+double  volumeEsfera = (4.0/3)  * (pi * Math.Pow(raio,3));
+
+Console.WriteLine($"VOLUME = {volumeEsfera.ToString("F3")}");
