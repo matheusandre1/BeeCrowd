45fa0a5e0ea10b01fe6c4ac254b9b5cd68259539
Matheus Andre <matheusandr2@gmail.com>
2024-10-11 21:45:21 -0300

    exercicio 1009

---
+++ BeeCrowd/1009/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Globalization;
+
+string name = Console.ReadLine();
+double salaryFix = double.Parse(Console.ReadLine());
+double totalSales = double.Parse(Console.ReadLine());
+double comission = 0.15;
+double moneycomission = comission * totalSales;
+
+double totalReceivableInTheMonth = salaryFix + moneycomission;
+
+Console.WriteLine($"TOTAL = {totalReceivableInTheMonth.ToString("F2",CultureInfo.InvariantCulture)}");
+
