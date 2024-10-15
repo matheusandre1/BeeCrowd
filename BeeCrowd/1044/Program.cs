4927199173f63fb4c4186f8e477504fc9e1a3012
Matheus Andre <matheusandr2@gmail.com>
2024-10-14 22:47:54 -0300

    exercicios 1044 e 1050

---
+++ BeeCrowd/1044/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+
+int num1 = int.Parse(valores[0]);
+int num2 = int.Parse(valores[1]);
+
+if (num1 % num2 != 0 && num2 % num1 != 0)
+{
+    Console.WriteLine("Nao sao Multiplos");
+}
+else
+{
+    Console.WriteLine("Sao Multiplos");
+}
