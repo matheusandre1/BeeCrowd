bb5f014da03b0c55b08950e7e004eac0e19c3283
Matheus Andre <matheusandr2@gmail.com>
2024-10-16 16:21:01 -0300

    feitos exercicio 1049, 1059, 1060

---
+++ BeeCrowd/1049/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+string frase = Console.ReadLine();
+ string frase2 = Console.ReadLine();
+ string frase3 = Console.ReadLine();
+
+if (frase == "vertebrado"  && frase2 == "ave" && frase3 == "carnivoro")
+ {
+    Console.WriteLine("aguia");
+ }
+
+ if (frase == "vertebrado"  && frase2 == "ave" && frase3 == "onivoro")
+ {
+    Console.WriteLine("pomba");
+ }
+
+ if (frase == "vertebrado"  && frase2 == "mamifero" && frase3 == "onivoro")
+ {
+    Console.WriteLine("homem");
+ }
+  if (frase == "vertebrado"  && frase2 == "mamifero" && frase3 == "herbivoro")
+ {
+    Console.WriteLine("vaca");
+ }
+
+ if (frase == "invertebrado" && frase2 == "inseto" && frase3 == "hematofago")
+ {
+    Console.WriteLine("pulga");
+ }
+
+ if (frase == "invertebrado" && frase2 == "inseto" && frase3 == "herbivoro")
+ {
+    Console.WriteLine("lagarta");
+ }
+
+ if (frase == "invertebrado" && frase2 == "anelideo" && frase3 == "hematofago")
+ {
+    Console.WriteLine("sanguessuga");
+ }
+
+ if (frase == "invertebrado" && frase2 == "anelideo" && frase3 == "onivoro")
+ {
+    Console.WriteLine("minhoca");
+ }
+ 
