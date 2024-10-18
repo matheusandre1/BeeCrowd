0c40d653b86745d762f2c0a30cd943cb4de164c3
Matheus Andre <matheusandr2@gmail.com>
2024-10-18 18:19:18 -0300

    adicionando 1079

---
+++ BeeCrowd/1116/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int quantidadeLidos = int.Parse(Console.ReadLine());
+
+int tentativas = 0;
+
+while( tentativas != quantidadeLidos)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+
+    double num1 = double.Parse(valores[0]);
+    double num2 = double.Parse(valores[1]);    
+
+    if ( num2 == 0)
+    {
+        Console.WriteLine("divisao impossivel");
+    }
+    else if ( num2 < 0)
+    {
+        double divisao = (double)num1 / (double)num2;
+        Console.WriteLine($"{divisao.ToString("0.0")}");
+    }
+    else
+    {
+        double divisao = (double)num1 / (double)num2;
+        Console.WriteLine(divisao.ToString("0.0"));
+    }
+
+   tentativas++;
+}
